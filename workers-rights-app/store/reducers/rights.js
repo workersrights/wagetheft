57aada9145f33fcbe0a59ee2635652eda69579f2
Manuka Stratta <manukavalentina@hotmail.com>
2020-04-16 19:09:00 -0700

    Added skeleton redux

---
+++ workers-rights-app/store/reducers/rights.js
@@ -0,0 +1,13 @@
+// manage state to remember favorite subrights information
+// skeleton, starter code
+
+// set up an initial state
+const initialState = {
+    rights: {},
+    favoriteRights: {}
+};
+
+
+const rightsReducer = (state = initialState, action) => {
+    return state;
+}
