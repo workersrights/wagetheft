30cdd46f4c35ae4a19e3d9162551cc5794174374
Manuka Stratta <manukavalentina@hotmail.com>
2020-04-16 21:24:01 -0700

    Started organization slider

--- workers-rights-app/screens/RightsDetailsScreen.js
+++ workers-rights-app/screens/RightsDetailsScreen.js
@@ -1,19 +1,35 @@
 import React from 'react';
-import { View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet, ScrollView} from 'react-native';
 import { SUBRIGHTS } from '../data/dummy-data';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import CustomHeaderButton from '../components/CustomHeaderButton';
+import OrganizationBox from '../components/OrganizationBox';
 
 const RightsDetailsScreen = props => {
     const subrightId = props.navigation.getParam('subrightId');
     const selectedSubright = SUBRIGHTS.find(subright => subright.id === subrightId);
 
     return(
-        <View style={styles.screen}>
+        <View style={{ height: 130, marginTop: 20 }}>
             <Text>The Rights Details Screen!</Text>
             <Text>{selectedSubright.title}</Text>
+            <ScrollView
+                horizontal={true}
+                showsHorizontalScrollIndicator={false}
+            >
+                <OrganizationBox image={require('../images/wage-theft-coalition-logo.png')}
+                    title="The Wage Theft Coalition"
+                />
+                <OrganizationBox image={require('../images/osha-logo.png')}
+                    title="OSHA"
+                />
+                <OrganizationBox image={require('../images/NLRB-logo.png')}
+                    title="PAWIS"
+                />
+            </ScrollView>
+
         </View>
-    );
+    )
 };
 
 RightsDetailsScreen.navigationOptions = navigationData => {
