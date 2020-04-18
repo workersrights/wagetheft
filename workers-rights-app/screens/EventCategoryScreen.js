e7c445c60a9bfc89fc73111e2d6648b0cf230d2b
vic-ayres <victoriaayresibarra@gmail.com>
2020-04-17 21:38:45 -0700

    Create EventCategoryScreen.js

---
+++ workers-rights-app/screens/EventCategoryScreen.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button} from 'react-native';
+
+const EventCategoryScreen = props => {
+    return(
+        <View style={styles.screen}>
+            <Text>The Event Category Screen!</Text>
+            <Button title='Go to Event Category Screen' onPress={() => props.navigation.navigate('EventDetails')} />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default EventCategoryScreen;
