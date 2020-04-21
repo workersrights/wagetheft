6d7b3bd734f107e22e3fccb2df3fffda925fcb5c
RyanRTJJ <ryan.rtjj@gmail.com>
2020-04-21 00:07:09 -0700

    Implemented modal

--- workers-rights-app/App.js
+++ workers-rights-app/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import RightsNavigator from './navigation/RightsNavigator';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import RightsNavigator from "./navigation/RightsNavigator";
+import { enableScreens } from "react-native-screens";
+
+enableScreens();
 
 export default function App() {
-  return (
-    <RightsNavigator />
-  );
+  return <RightsNavigator />;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- workers-rights-app/components/RightsCategoryTile.js
+++ workers-rights-app/components/RightsCategoryTile.js
@@ -1,37 +1,47 @@
-import React from 'react';
-import { TouchableOpacity, View, Text, StyleSheet, Image } from 'react-native';
-import Colors from '../constants/Colors';
+import React from "react";
+import { TouchableOpacity, View, Text, StyleSheet, Image } from "react-native";
+import Colors from "../constants/Colors";
 
-const RightsCategoryTile = props => {
-    return (
-        <TouchableOpacity style={styles.gridItem} onPress={props.onSelect}>
-            <View>
-                <Image style={styles.imageStyle} source={props.image} />
-                <Text>{props.title}</Text>
-            </View>
-        </TouchableOpacity>
-    );
+const RightsCategoryTile = (props) => {
+  return (
+    <TouchableOpacity style={styles.gridItem} onPress={props.onSelect}>
+      <View style={styles.container}>
+        <Image style={styles.imageStyle} source={props.image} />
+        <View style={styles.textContainer}>
+          <Text>{props.title}</Text>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
 };
 
 const styles = StyleSheet.create({
-    gridItem: {
-        height: 150,
-        width: 150,
-        margin: 10,
-        backgroundColor: Colors.lightOrange,
-        borderRadius: 15,
-        shadowOpacity: 0.25,
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: 2 },
-        shadowRadius: 6,
-        elevation: 3,
-        padding: 10,
-        justifyContent: 'flex-end',
-        alignItems: 'center'
-    },
-    imageStyle: { // TODO: fix this. Make the image appear higher in the tile/box.
-        justifyContent: 'center'
-    }
+  gridItem: {
+    height: 150,
+    width: 150,
+    margin: 10,
+    backgroundColor: Colors.lightOrange,
+    borderRadius: 15,
+    shadowOpacity: 0.25,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    elevation: 3,
+  },
+  container: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imageStyle: {
+    position: "absolute",
+    top: "20%",
+  },
+  textContainer: {
+    position: "absolute",
+    bottom: "10%",
+  },
 });
 
 export default RightsCategoryTile;
