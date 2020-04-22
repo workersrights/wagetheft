220fa6c39b0ea60375be87a34478a0c5563e698b
vic-ayres <victoriaayresibarra@gmail.com>
2020-04-21 20:20:37 -0700

    Update EventCategoryScreen.js

--- workers-rights-app/screens/EventCategoryScreen.js
+++ workers-rights-app/screens/EventCategoryScreen.js
@@ -1,21 +1,58 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button} from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+
+import { EVENTS } from "../data/dummy-data";
+import EventCategoryCard from '../components/EventCategoryCard';
 
 const EventCategoryScreen = props => {
-    return(
-        <View style={styles.screen}>
-            <Text>The Event Category Screen!</Text>
-            <Button title='Go to Event Detail Screen' onPress={() => props.navigation.navigate('EventDetails')} />
-        </View>
+    const renderCards = (itemData) => {
+        return (
+          <EventCategoryCard
+            title={itemData.item.title}
+            date={itemData.item.date}
+            time={itemData.item.time}
+            location={itemData.item.location}
+            image={itemData.item.image}
+            pressAction={(id) => {
+                props.navigation.navigate({
+                  routeName: "EventDetails",
+                  params: {
+                    eventId: id,
+                  },
+                }) }
+            }
+            id={itemData.item.id}
+          />
+        );
+      };
+    
+    {/* validEvents isn't going to be used yet */}
+    const validEvents = EVENTS.filter(
+        e => e.category === props.category);
+    return (
+       <View style={styles.container}>
+         <FlatList
+           data={EVENTS}
+           renderItem={renderCards}
+           showsVerticalScrollIndicator={false}
+         />
+       </View>
     );
+}    
+const styles = StyleSheet.create({      
+    container: {
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 10,
+    paddingHorizontal: 10
+    }
+});
+
+
+// Sets the header title to the category title
+EventCategoryScreen.navigationOptions = (navigationData) => {
+    return{headerTitle: navigationData.navigation.getParam('categoryTitle')};
 };
 
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-})
 
 export default EventCategoryScreen;
