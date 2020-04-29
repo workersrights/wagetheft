0074905c359c273b764d6cb1466d5e3ef61c5510
vic-ayres <victoriaayresibarra@gmail.com>
2020-04-28 20:53:25 -0700

    Update EventDetailsScreen.js

--- workers-rights-app/screens/EventDetailsScreen.js
+++ workers-rights-app/screens/EventDetailsScreen.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
 import EventDetails from "../components/EventDetails";
+import { useSelector, useDispatch } from 'react-redux';
+import { Ionicons } from '@expo/vector-icons';
+
 
 const EventDetailsScreen = (props) => {
   return (
@@ -8,6 +11,25 @@ const EventDetailsScreen = (props) => {
       <EventDetails id={props.navigation.getParam("eventId")} />
     </View>
   );
+};
+
+EventDetailsScreen.navigationOptions = (navigationData) => {
+  const eventId = navigationData.navigation.getParam('eventId');
+  const availableEvents = useSelector(state => state.events.allEvents);
+  const event = availableEvents.find(event => event.id===eventId);
+  //const toggleFavorite = navigationData.navigation.getParam('toggleFav');
+  //const isFav = navigationData.navigation.getParam('isFav');
+  return{
+      headerTitle: event.title,
+      headerRight: () => {
+        <Ionicons
+          size={23} 
+          color='black'
+          name={'ios-star'}//isFav?'ios-star':'ios-star-outline'}
+          //onPress={toggleFavorite}
+        />
+      }
+  };
 };
 
 const styles = StyleSheet.create({
