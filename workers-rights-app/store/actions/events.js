e97388c53fc406dd8f16191ad83f6539820f9434
vic-ayres <victoriaayresibarra@gmail.com>
2020-04-30 19:21:31 -0700

    Update events.js

--- workers-rights-app/store/actions/events.js
+++ workers-rights-app/store/actions/events.js
@@ -1,5 +1,20 @@
 export const SET_YOUR_EVENT = 'SET_YOUR_EVENT';
 
 export const setYourEvent = eventId => {
-    return { type: SET_YOUR_EVENT, id: eventId};
+    return async dispatch => {
+        const response = await fetch('https://wage-theft-6c007.firebaseio.com/events.json', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(eventId)
+        });
+        const resData = await response.json();
+        console.log(resData);
+
+        dispatch({ 
+            type: SET_YOUR_EVENT, 
+            id: eventId
+        });
+    }
 };
