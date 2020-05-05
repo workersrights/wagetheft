e57637dbd4d6e3aa057ca99f37d0ddf5120196ee
Angel Pan <angelpan@pop-os.localdomain>
2020-05-06 07:19:19 +0800

    restructure org modal with new design to match the rest of app

--- workers-rights-app/components/RightsOrganizationModal.js
+++ workers-rights-app/components/RightsOrganizationModal.js
@@ -1,42 +1,54 @@
 import React from "react";
-import {ScrollView, View, Text, Image, StyleSheet, Button } from "react-native";
+import {Dimensions, ScrollView, View, Text, Image, StyleSheet, Button } from "react-native";
 import Colors from "../constants/Colors";
-import ModalCloseButton from "./ModalCloseButton.js";
 import {ORGANIZATIONS } from "../data/dummy-data";
 import Modal from 'react-native-modal';
 
-{/*const RightsOrganizationModal = (props) => {
+const RightsOrganizationModal = (props) => {
   const selectedOrganization = ORGANIZATIONS.filter(
     (organization) => organization.id === props.organizationId
   )[0];
 
+  const deviceWidth = Dimensions.get("window").width;
+  const deviceHeight = Dimensions.get("window").height;
+  const changeSize = (contentWidth, contentHeight) => {
+    styles.scroll = {
+      height: contentHeight,
+      width: contentWidth
+    }
+  };
   return (
-    <Modal visible={props.isVisible} transparent={true} animationType="fade">
-      <View style={styles.dimOverlay}>
-        <ScrollView style={styles.modal}>
-          <View style={styles.modalContent}>
-            <View style={styles.imageContainer}>
-                {// image here }
-                <Image
-                  source={selectedOrganization.image}
-                  resizeMode="contain"
-                  style={styles.image}
-                />
-            </View>
-            <ModalCloseButton onCloseModal={props.onCloseModal} />
-            <Text style={styles.title}>{selectedOrganization.title}</Text>
-            {// Subtitles and description here }
-            <View style={styles.description}>
-              {selectedOrganization.description.map(desc => (
-                <View style={styles.subheading}>
-                    {console.log(desc)}
-                    <Text style={styles.subtitle}>{desc.title}</Text>
-                    {desc.data.map(line => (
-                      <Text style={styles.info}>{line}</Text>
-                    ))}
-                </View>
-              ))}
-            </View>
+    <Modal 
+      isVisible={props.isVisible} 
+      deviceHeight={deviceHeight}
+      deviceWidth={deviceWidth}
+      propagateSwipe
+      onBackButtonPress={props.onCloseModal}
+      onBackdropPress={props.onCloseModal}
+      >
+      <View style={styles.modal}>
+        <ScrollView contentContainerStyle={styles.scroll} >
+          <Text style={styles.title}>{selectedOrganization.title}</Text>
+          <View style={styles.imageContainer}>
+              {/* image here */ }
+              <Image
+                source={selectedOrganization.image}
+                resizeMode="contain"
+                style={styles.image}
+              />
+          </View>
+          
+          {/* Subtitles and description here */ }
+          <View style={styles.description}>
+            {selectedOrganization.description.map(desc => (
+              <View style={styles.subheading}>
+                  {console.log(desc)}
+                  <Text style={styles.subtitle}>{desc.title}</Text>
+                  {desc.data.map(line => (
+                    <Text style={styles.info}>{line}</Text>
+                  ))}
+              </View>
+            ))}
           </View>
         </ScrollView>
       </View>
@@ -47,34 +59,25 @@ import Modal from 'react-native-modal';
 const styles = StyleSheet.create({
   modal: {
     width: "80%",
-    height: "50%",
+    height: "80%",
     backgroundColor: Colors.lightOrange,
-    borderRadius: 0,
     shadowOpacity: 0.25,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     elevation: 3,
+    alignSelf: "center",
+    borderRadius: 8,
   },
-  dimOverlay: {
-    flex: 1,
-    backgroundColor: "rgba(0,0,0,0.26)",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  modalContent: {
-    flex: 1,
-    alignItems: "center",
+  scroll: {
+    paddingBottom: 400
   },
   title: {
-    paddingTop: "30%",
-    paddingHorizontal: 10,
+    padding: 10,
+    paddingBottom: 0,
     fontWeight: "bold",
-    fontSize: 30,
-    textAlign: "center",
-    color: "white",
-    textShadowColor: "#000000",
-    textShadowRadius: 10
+    fontSize: 16,
+    textAlign: "center"
   },
   subtitle: {
     textAlign: "center",
@@ -84,7 +87,9 @@ const styles = StyleSheet.create({
     padding: 5
   },
   description: {
-    padding: 15,
+    flex:1,
+    padding: 10,
+    paddingTop: 0,
     textAlign: "center",
   },
   info :{
@@ -92,41 +97,16 @@ const styles = StyleSheet.create({
     fontSize: 12,
   },
   imageContainer: {
-    position: "absolute",
-    width: "100%",
-    height: "40%",
+    height: "50%",
+    padding: 5
   },
   image: {
     flex: 1, 
-    width: null,
-    height: null, 
-    resizeMode: 'cover',
+    width: "100%",
+    height: "100%", 
+    resizeMode: "contain",
     borderRadius: 8
   },
-});*/}
+});
 
-export default class ModalTester extends React.Component {
-  state = {
-    isModalVisible: false,
-  };
-
-  toggleModal = () => {
-    this.setState({isModalVisible: !this.state.isModalVisible});
-  };
-
-  render() {
-    return (
-      <View style={{flex: 1}}>
-        <Button title="Show modal" onPress={this.toggleModal} />
-        <Modal isVisible={this.state.isModalVisible}>
-          <View style={{flex: 1}}>
-            <Text>Hello!</Text>
-            <Button title="Hide modal" onPress={this.toggleModal} />
-          </View>
-        </Modal>
-      </View>
-    );
-  }
-}
-
-//export default RightsOrganizationModal;
+export default RightsOrganizationModal;
