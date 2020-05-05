b6f458e7f866f3ab43d662b1746f4b94ede63cb4
Angel Pan <angelpan@pop-os.localdomain>
2020-05-06 07:40:24 +0800

    change category model to have the same ui

--- workers-rights-app/components/RightsCategoryModal.js
+++ workers-rights-app/components/RightsCategoryModal.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { View, Text, Modal, Image, StyleSheet, Dimensions } from "react-native";
+import { View, Text, Image, StyleSheet, Dimensions } from "react-native";
 import Colors from "../constants/Colors";
 import ModalCloseButton from "./ModalCloseButton.js";
 import { RIGHTSCATEGORIES } from "../data/dummy-data";
 import ButtonTemplate from "../components/ButtonTemplate";
+import Modal from "react-native-modal"
 
 const RightsCategoryModal = (props) => {
   const selectedCategory = RIGHTSCATEGORIES.filter(
@@ -11,12 +12,13 @@ const RightsCategoryModal = (props) => {
   )[0];
 
   return (
-    <Modal visible={props.isVisible} transparent={true} animationType="fade">
-      <View style={styles.dimOverlay}>
+    <Modal 
+      isVisible={props.isVisible} 
+      onBackButtonPress={props.onCloseModal}
+      onBackdropPress={props.onCloseModal}
+      >
         <View style={styles.modal}>
           <View style={styles.modalContent}>
-            <ModalCloseButton onCloseModal={props.onCloseModal} />
-
             <Text style={styles.title}>{selectedCategory.title}</Text>
             {/* image here */}
             <Image
@@ -33,7 +35,6 @@ const RightsCategoryModal = (props) => {
             <ButtonTemplate onPress={props.onAdvance} />
           </View>
         </View>
-      </View>
     </Modal>
   );
 };
@@ -41,20 +42,15 @@ const RightsCategoryModal = (props) => {
 const styles = StyleSheet.create({
   modal: {
     width: "80%",
-    height: 530, // Hard-coded value to fit iPhone 6/7/8. Dynamic sizing, anyone?
+    height: "80%", // Hard-coded value to fit iPhone 6/7/8. Dynamic sizing, anyone?
     backgroundColor: Colors.lightOrange,
-    borderRadius: 0,
+    borderRadius: 8,
     shadowOpacity: 0.25,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     elevation: 3,
-  },
-  dimOverlay: {
-    flex: 1,
-    backgroundColor: "rgba(0,0,0,0.26)",
-    justifyContent: "center",
-    alignItems: "center",
+    alignSelf: "center"
   },
   modalContent: {
     flex: 1,
