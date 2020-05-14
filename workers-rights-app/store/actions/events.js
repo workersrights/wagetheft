a79af0ca2dea1fcceb3b6b5fd77af49416c8d9f4
vic-ayres <victoriaayresibarra@gmail.com>
2020-05-14 16:35:22 -0700

    Update events.js

--- workers-rights-app/store/actions/events.js
+++ workers-rights-app/store/actions/events.js
@@ -1,13 +1,32 @@
+import { useSelector } from 'react-redux'; 
 export const SET_YOUR_EVENT = 'SET_YOUR_EVENT';
 
-export const setYourEvent = eventId => {
+export const setYourEvent = (
+    title,
+    date,
+    time,
+    image,
+    organizer,
+    location,
+    price,
+    category,
+    description) => {
     return async dispatch => {
-        const response = await fetch('https://wage-theft-6c007.firebaseio.com/events.json', {
+        const response = await fetch('https://workers-rights-46c43.firebaseio.com/events', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify(eventId)
+            body: JSON.stringify({
+                title,
+                date,
+                time,
+                image,
+                organizer,
+                location,
+                price,
+                category,
+                description})
         });
         const resData = await response.json();
         console.log(resData);
