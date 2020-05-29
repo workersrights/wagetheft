44ce97c6ce008f24ee9454f26988e7ebaf4ea119
vic-ayres <victoriaayresibarra@gmail.com>
2020-05-28 19:14:12 -0700

    Update event.js

--- workers-rights-app/models/event.js
+++ workers-rights-app/models/event.js
@@ -18,6 +18,28 @@ class Event {
     this.category = category;
     this.description = description;
   }
+
+  Date = (formatStr = "MMMM Mo, YYYY") => {
+    var moment = require('moment');
+    const date = moment(this.date).format(formatStr);
+    return date.toString();
+  };
+  
+  Time = () => {
+    var moment = require('moment');
+    const date = moment(this.date).format("LT");
+    return date.toString();
+  };
 }
 
 export default Event;
+
+export const compareEvents = (e1, e2) => {
+  var moment = require('moment');
+  const date1 = moment(e1.date);
+  const date2 = moment(e2.date);
+  if(date1.isSame(date2))
+    return 0;
+
+  return (date1.isBefore(date2)) ? -1 : 1;
+}
