0ee29d341eae1404e53e5ab4927a2163d8875d8b
Manuka Stratta <manukavalentina@hotmail.com>
2020-06-03 00:20:44 -0700

    Implemented sorting tweets by date (moment, comparison function)

--- workers-rights-app/data/GetRightsTweets.js
+++ workers-rights-app/data/GetRightsTweets.js
@@ -23,30 +23,48 @@ export default class GetRightsTweets {
         var twitterObj = twitter(TWITTERKEYS);
         var tempArr = [];
 
-        let screenNames = ["NLRB", "CA_EDD", "USDOL"];
+        let screenNames = ["NLRB", "CA_EDD", "USDOL", "CalDFEH"];
+        let numRecentTweets = 4;
         for(var name of screenNames) {
-            const res = await getUserRecentTweets(twitterObj, name, 4);
+            const res = await getUserRecentTweets(twitterObj, name, numRecentTweets);
             tempArr = [...tempArr, ...res]
         }
-        shuffleArray(tempArr);
+        tempArr.sort(compareTweets);
         GetRightsTweets.setLoadedTweets(tempArr);
     }
 }
 
 async function getUserRecentTweets(twitterObj, screenName, count) {
     var tempArr = [];
+    var moment = require('moment');
     var myPromise = twitterObj.rest.get('statuses/user_timeline', {screen_name: screenName, count: count, tweet_mode: "extended"})
     .then(console.log("done!"));
     return myPromise.then((info) => {
         for(var x of info) {
-            var temp = new RightsTweet(x.id_str, x.user.name, x.user.screen_name, x.user.profile_image_url, x.full_text, x.created_at);
+            var temp = new RightsTweet(
+                x.id_str,
+                x.user.name,
+                x.user.screen_name,
+                x.user.profile_image_url,
+                x.full_text,
+                x.created_at
+                );
             tempArr.push(temp);
         }
         return tempArr;
     });
 }
 
-// TODO. temp solution; will need to sort the tweets array chrnologically
+const compareTweets = (tweetA, tweetB) => {
+    let INPUT_FORMAT = "ddd MMM DD HH:mm:ss ZZ YYYY"; // Twitter API format
+    var moment = require('moment');
+    const dateA = moment(tweetA.time, INPUT_FORMAT);
+    const dateB = moment(tweetB.time, INPUT_FORMAT);
+    if(dateA.isSame(dateB))
+      return 0;
+    return (dateA.isBefore(dateB)) ? 1 : -1;
+}
+
 function shuffleArray(array) { 
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
