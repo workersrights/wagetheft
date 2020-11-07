9417c5a20e88d9b03a7fe27bc15767ddb684dfed
Alex Oseguera <aoseg@stanford.edu>
2020-11-07 02:50:50 -0800

    Cleaned up and commented RightsDetailsScreen.js

--- workers-rights-app/screens/RightsDetailsScreen.js
+++ workers-rights-app/screens/RightsDetailsScreen.js
@@ -1,161 +1,185 @@
 import React, { useState, useRef } from "react";
 import { View, Text, StyleSheet, ScrollView, FlatList } from "react-native";
-import { HeaderButtons, Item } from "react-navigation-header-buttons";
-import CustomHeaderButton from "../components/CustomHeaderButton";
 import OrganizationBox from "../components/OrganizationBox";
 import LearnMoreItem from "../components/LearnMoreItem";
 import RightsSheetContent from "../components/RightsSheetContent";
-import { Modalize } from "react-native-modalize"; // Credits to https://github.com/jeremybarbet/react-native-modalize
+import { Modalize } from "react-native-modalize";
 import { Portal } from "react-native-portalize";
 import Colors from "../constants/Colors";
 import RightsOrganizationModal from "../components/RightsOrganizationModal";
 import ImportedData from "../data/FetchRightsData";
 
+/*
+ *
+ * Functional Component Defintion: RightsDetailsScreen
+ *
+ * Displays RightsDetailScreen
+ *
+ */
+
 const RightsDetailsScreen = (props) => {
-  // Get the parent subright
-  const parentSubRightId = props.navigation.getParam("subrightId");
-  const parentSubRight = ImportedData.getSubRights().find(
-    (subRight) => subRight.id === parentSubRightId
-  );
-  // Get list of LearnMore Ids to display. Works on empty array as well.
-  const displayedLearnMoreIds = parentSubRight.learnMores
-    ? parentSubRight.learnMores
-    : []; // if empty
+	// Get the parent subright
+	const parentSubRightId = props.navigation.getParam("subrightId");
+	const parentSubRight = ImportedData.getSubRights().find(
+		(subRight) => subRight.id === parentSubRightId
+	);
+	// Get list of LearnMore Ids to display. Works on empty array as well.
+	const displayedLearnMoreIds = parentSubRight.learnMores
+		? parentSubRight.learnMores
+		: [];
 
-  // Get list of relevant orgs to this specific subright
-  const relevantOrgs = (parentSubRight.organizations === undefined) ? [] : ImportedData.getOraganizations().filter((org) =>
-    parentSubRight.organizations.includes(org.id)
-  );
+	// Get list of relevant orgs to this specific subright
+	const relevantOrgs =
+		parentSubRight.organizations === undefined
+			? []
+			: ImportedData.getOraganizations().filter((org) =>
+					parentSubRight.organizations.includes(org.id)
+			  );
 
-  // org modal stuff
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [activeOrganizationId, setActiveOrganizationId] = useState("");
+	const [isModalOpen, setIsModalOpen] = useState(false);
+	const [activeOrganizationId, setActiveOrganizationId] = useState("");
 
-  const openModalHandler = (id) => {
-    setIsModalOpen(true);
-    setActiveOrganizationId(id);
-  };
+	const openModalHandler = (id) => {
+		setIsModalOpen(true);
+		setActiveOrganizationId(id);
+	};
 
-  const closeModalHandler = () => {
-    setIsModalOpen(false);
-  };
+	const closeModalHandler = () => {
+		setIsModalOpen(false);
+	};
 
-  // State Hooks for LearnMore modals.
-  const [activeLearnMoreId, setActiveLearnMoreId] = useState(
-    "-M7LY3fU0-iSv0HX94zB"
-  ); // Corresponds to lm1. Needs to be initialized with any garbage learnMore, otherwise app will crash because
-  const modalizeRef = useRef(null);
+	// Corresponds to lm1. Needs to be initialized with any garbage learnMore, otherwise app will crash because
+	const [activeLearnMoreId, setActiveLearnMoreId] = useState(
+		"-M7LY3fU0-iSv0HX94zB"
+	);
+	const modalizeRef = useRef(null);
 
-  const renderOrgItem = (itemData) => {
-    return (
-      <OrganizationBox
-        title={itemData.item.name}
-        image={itemData.item.image}
-        onSelect={() => {
-          openModalHandler(itemData.item.id);
-        }}
-      />
-    );
-  };
+	/*
+	 *
+	 * Function is called to render OrganizationBox components
+	 * in a horizontal FlatList
+	 *
+	 */
 
-  const openLearnMoreHandler = (id) => {
-    setActiveLearnMoreId(id);
-    modalizeRef.current?.open();
-  };
+	const renderOrgItem = (itemData) => {
+		return (
+			<OrganizationBox
+				title={itemData.item.name}
+				image={itemData.item.image}
+				onSelect={() => {
+					openModalHandler(itemData.item.id);
+				}}
+			/>
+		);
+	};
 
-  return (
-    <View style={styles.screen}>
-      <ScrollView
-        style={{
-          paddingLeft: 20,
-          paddingRight: 20,
-          width: "100%",
-        }}
-        showsVerticalScrollIndicator={false}
-      >
-        {/* Initial subright description */}
-        <Text style={styles.section}>Description: </Text>
-        <Text>{parentSubRight.description}</Text>
+	const openLearnMoreHandler = (id) => {
+		setActiveLearnMoreId(id);
+		modalizeRef.current?.open();
+	};
 
-        {/* Organization section */}
-        <Text style={styles.section}>
-          Contact the following agencies for help:{" "}
-        </Text>
-        <Text>(Tap on an organization for more information)</Text>
-        <View style={{ height: 230, marginTop: 20 }}>
-          <FlatList
-            data={relevantOrgs}
-            renderItem={renderOrgItem}
-            horizontal={true}
-            showsHorizontalScrollIndicator={false}
-          />
-        </View>
+	return (
+		<View style={styles.screen}>
+			<ScrollView
+				style={styles.scrollViewStyle}
+				showsVerticalScrollIndicator={false}
+			>
+				<Text style={styles.section}>Description: </Text>
+				<Text>{parentSubRight.description}</Text>
 
-        {/* ------------- LEARN MORE SECTION -------------- */}
-        <Text style={styles.section}>Learn more:</Text>
-        {displayedLearnMoreIds.map((displayedLearnMoreId) => (
-          <LearnMoreItem
-            id={displayedLearnMoreId}
-            key={displayedLearnMoreId}
-            onPress={() => {
-              openLearnMoreHandler(displayedLearnMoreId);
-            }}
-          />
-        ))}
-      </ScrollView>
+				<Text style={styles.section}>
+					Contact the following agencies for help:{" "}
+				</Text>
+				<Text>(Tap on an organization for more information)</Text>
+				<View style={styles.orgContainer}>
+					<FlatList
+						data={relevantOrgs}
+						renderItem={renderOrgItem}
+						horizontal={true}
+						showsHorizontalScrollIndicator={false}
+					/>
+				</View>
 
-      {/* --------------- Org Modal Section --------------- */}
-      {activeOrganizationId !== "" && (
-        <RightsOrganizationModal
-          isVisible={isModalOpen}
-          onCloseModal={closeModalHandler}
-          id={activeOrganizationId}
-        ></RightsOrganizationModal>
-      )}
+				<Text style={styles.section}>Learn more:</Text>
+				{displayedLearnMoreIds.map((displayedLearnMoreId) => (
+					<LearnMoreItem
+						id={displayedLearnMoreId}
+						key={displayedLearnMoreId}
+						onPress={() => {
+							openLearnMoreHandler(displayedLearnMoreId);
+						}}
+					/>
+				))}
+			</ScrollView>
 
-      <Portal>
-        <Modalize ref={modalizeRef} modalStyle={styles.modalize}>
-          <View style={styles.modalizeContent}>
-            <RightsSheetContent learnMoreId={activeLearnMoreId} />
-          </View>
-        </Modalize>
-      </Portal>
-    </View>
-  );
+			{activeOrganizationId !== "" && (
+				<RightsOrganizationModal
+					isVisible={isModalOpen}
+					onCloseModal={closeModalHandler}
+					id={activeOrganizationId}
+				/>
+			)}
+
+			<Portal>
+				<Modalize ref={modalizeRef} modalStyle={styles.modalize}>
+					<View style={styles.modalizeContent}>
+						<RightsSheetContent learnMoreId={activeLearnMoreId} />
+					</View>
+				</Modalize>
+			</Portal>
+		</View>
+	);
 };
 
-RightsDetailsScreen.navigationOptions = (navigationData) => {
-  const subrightId = navigationData.navigation.getParam("subrightId");
-  const parentSubRight = ImportedData.getSubRights().find(
-    (subright) => subright.id === subrightId
-  );
+/*
+ *
+ * Sets the header of the RightsDetailScreen to the category
+ * title associated with the subrightID passed in from the
+ * SubRightsScreen
+ *
+ */
 
-  if (parentSubRight.title.length > 25) {
-    return {
-      headerTitle: parentSubRight.title.substring(0, 21) + "...",
-      headerBackTitle: "Back",
-    };
-  } else {
-    return { headerTitle: parentSubRight.title, headerBackTitle: "Back" };
-  }
+RightsDetailsScreen.navigationOptions = (navigationData) => {
+	const subrightId = navigationData.navigation.getParam("subrightId");
+	const parentSubRight = ImportedData.getSubRights().find(
+		(subright) => subright.id === subrightId
+	);
+
+	if (parentSubRight.title.length > 25) {
+		return {
+			headerTitle: parentSubRight.title.substring(0, 21) + "...",
+			headerBackTitle: "Back",
+		};
+	} else {
+		return { headerTitle: parentSubRight.title, headerBackTitle: "Back" };
+	}
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-  },
-  section: {
-    fontSize: 16,
-    fontWeight: "700",
-    marginTop: 20,
-  },
-  modalize: {
-    height: "85%",
-    backgroundColor: Colors.lightOrange,
-  },
-  modalizeContent: {
-    flex: 1,
-  },
+	screen: {
+		flex: 1,
+	},
+	section: {
+		fontSize: 16,
+		fontWeight: "700",
+		marginTop: 20,
+	},
+	modalize: {
+		height: "85%",
+		backgroundColor: Colors.lightOrange,
+	},
+	modalizeContent: {
+		flex: 1,
+	},
+	orgContainer: {
+		height: 230,
+		marginTop: 20,
+	},
+	scrollViewStyle: {
+		paddingLeft: 20,
+		paddingRight: 20,
+		width: "100%",
+	},
 });
 
 export default RightsDetailsScreen;
