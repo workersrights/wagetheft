4ec7c5f8b8a505e2865bfe2cee3c26e5380b627e
Alex Oseguera <aoseg@stanford.edu>
2020-11-07 03:39:31 -0800

    Refractored App.js to have all loading items in a single asynchronous function

--- workers-rights-app/App.js
+++ workers-rights-app/App.js
@@ -3,64 +3,52 @@ import RightsNavigator from "./navigation/RightsNavigator";
 import { Host } from "react-native-portalize";
 import * as Font from "expo-font";
 import { AppLoading } from "expo";
-import { enableScreens } from 'react-native-screens';
-import ImportedData from './data/FetchRightsData.js'; 
+import { enableScreens } from "react-native-screens";
+import ImportedData from "./data/FetchRightsData.js";
 
 /*
- * 
- * Provides native primitives to represent screens instead of 
- * plain <View> components in order to better take advantage of 
+ *
+ * Provides native primitives to represent screens instead of
+ * plain <View> components in order to better take advantage of
  * operating system behavior and optimizations around screens
- * 
+ *
  */
 
 enableScreens();
 
 /*
- * 
- * Asynchronous function to import data into rights from 
- * Friebase. 
- * 
+ *
+ * Asynchronous function to import data into rights from
+ * Firebase and load all fonts. The function waits until the loading
+ * process is complete by waiting until all promises are resolved.
+ *
  */
 
 async function loadAllData() {
-  fetchFonts();
-  await ImportedData.importAllData();
+	await Font.loadAsync({
+		"nunito-light": require("./assets/fonts/Nunito-Light.ttf"),
+		"nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+		"nunito-semibold": require("./assets/fonts/Nunito-SemiBold.ttf"),
+		"nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+		"nunito-extrabold": require("./assets/fonts/Nunito-ExtraBold.ttf"),
+	});
+	await ImportedData.importAllData();
 }
-
-/*
- * 
- * Function loads the functions into the app by calling the 
- * Expo Font.loadAsync function which returns a promise 
- * that is resolved after fetching is complete
- * 
- */
-
-const fetchFonts = () => {
-  Font.loadAsync({
-    "nunito-light": require("./assets/fonts/Nunito-Light.ttf"),
-    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
-    "nunito-semibold": require("./assets/fonts/Nunito-SemiBold.ttf"),
-    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
-    "nunito-extrabold": require("./assets/fonts/Nunito-ExtraBold.ttf"),
-  });
-};
 
 export default function App() {
-  const [dataLoaded, setDataLoaded] = useState(false);
-  if (!dataLoaded) {
-    return (
-      <AppLoading
-        startAsync={loadAllData}
-        onFinish={() => setDataLoaded(true)}
-      />
-    );
-  }
+	const [dataLoaded, setDataLoaded] = useState(false);
+	if (!dataLoaded) {
+		return (
+			<AppLoading
+				startAsync={loadAllData}
+				onFinish={() => setDataLoaded(true)}
+			/>
+		);
+	}
 
-  return (
-    <Host >
-        <RightsNavigator />
-    </Host>
-  );
+	return (
+		<Host>
+			<RightsNavigator />
+		</Host>
+	);
 }
-
