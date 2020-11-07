67f32ec7cf5338ceb88ecd45c95cb082f5187169
Alex Oseguera <aoseg@stanford.edu>
2020-11-07 02:52:31 -0800

    Reformatted model classes

--- workers-rights-app/models/organization.js
+++ workers-rights-app/models/organization.js
@@ -1,12 +1,12 @@
 class Organization {
-    constructor(id, name, abbrev, image, description, rights) {
-        this.id = id;
-        this.name = name;
-        this.abbrev = abbrev;
-        this.image = image;
-        this.description = description;
-        this.rights = rights; // array of rights the agency helps with
-    }
+	constructor(id, name, abbrev, image, description, rights) {
+		this.id = id;
+		this.name = name;
+		this.abbrev = abbrev;
+		this.image = image;
+		this.description = description;
+		this.rights = rights;
+	}
 }
 
 export default Organization;
--- workers-rights-app/models/subRight.js
+++ workers-rights-app/models/subRight.js
@@ -1,14 +1,23 @@
 class SubRight {
-  constructor(id, categoryIds, title, img, emoji, learnMores, description, organizations) {
-    this.id = id;
-    this.categoryIds = categoryIds;
-    this.title = title;
-    this.img = img;
-    this.emoji = emoji;
-    this.learnMores = learnMores;
-    this.description = description;
-    this.organizations = organizations;
-  }
+	constructor(
+		id,
+		categoryIds,
+		title,
+		img,
+		emoji,
+		learnMores,
+		description,
+		organizations
+	) {
+		this.id = id;
+		this.categoryIds = categoryIds;
+		this.title = title;
+		this.img = img;
+		this.emoji = emoji;
+		this.learnMores = learnMores;
+		this.description = description;
+		this.organizations = organizations;
+	}
 }
 
 export default SubRight;
