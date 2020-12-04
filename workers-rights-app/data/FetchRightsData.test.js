b11d004fdcbab3b537075ae7bf61f262b6806651
Manuka Stratta <manukavalentina@hotmail.com>
2020-12-04 02:25:21 +0100

    updated data fetching unit tests

--- workers-rights-app/data/FetchRightsData.test.js
+++ workers-rights-app/data/FetchRightsData.test.js
@@ -4,8 +4,11 @@ import SubRight from "../models/subRight";
 import Organization from "../models/organization";
 import learnMore from "../models/learnMore";
 
-test('Test empty initial state of ImportedData', () => {
-    expect(ImportedData.getRightsCategories()).toStrictEqual([]);
+test('Test empty initial state of ImportedData before calling importAllData', () => {
+    expect(ImportedData.getRightsCategories()).toStrictEqual([]); // should be the empty array
+    expect(ImportedData.getSubRights()).toStrictEqual([]);
+    expect(ImportedData.getOraganizations()).toStrictEqual([]);
+    expect(ImportedData.getLearnMores()).toStrictEqual([]);
   });
 
 test('Test that importAllData populates data structures', () => {
@@ -23,16 +26,16 @@ test('Test types of ImportedData data structure elements', () => {
     let orgs = ImportedData.getOraganizations();
     let learnmores = ImportedData.getLearnMores();
 
-    isInstanceOf(rightsCat, RightsCategory);
-    isInstanceOf(subrights, SubRight);
-    isInstanceOf(orgs, Organization);
-    isInstanceOf(learnmores, learnMore);
+    containsInstancesOf(rightsCat, RightsCategory); // all elements of rightsCat should be instances of class RightsCategory
+    containsInstancesOf(subrights, SubRight);
+    containsInstancesOf(orgs, Organization);
+    containsInstancesOf(learnmores, learnMore);
     // expect(orgs[0] instanceof Organization).toBe(true);
 });
 
 // Checks whether all elements of lst array is an instance of the specified espected class.
 // For example, all elements of ImportedData.getRightsCategories() should be instances of class RightsCategory.
-function isInstanceOf(lst, expectedClass) {
+function containsInstancesOf(lst, expectedClass) {
     let len = lst.length;
     for(var i = 0; i < len; i++) {
         expect(lst[i] instanceof expectedClass).toBe(true);
