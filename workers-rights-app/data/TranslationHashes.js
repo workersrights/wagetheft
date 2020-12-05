c74523a9d990dea4968845d64c833773c9da3e2c
Manuka Stratta <manukavalentina@hotmail.com>
2020-12-05 03:47:32 +0100

    initial hash attemp

---
+++ workers-rights-app/data/TranslationHashes.js
@@ -0,0 +1,8 @@
+const data = require('./workers-rights-46c43-export.json');
+//console.log(data);
+// const len = data.size();
+// console.log("len: ", len);
+// for(var i = 0; i < len; i++) {
+//     console.log(data[i]);
+// }
+console.log(data["rights-categories"]);
