774ef3e7e37b89123a1032dedbf67d7d899b585d
Manuka Stratta <manukavalentina@hotmail.com>
2020-12-15 02:14:38 +0100

    Add multi-language support in App (#43)
    * fixed dependency issues
    
    * added multi language support
    
    * initial hash attemp
    
    * removing inefficient language structure
    
    * Added support for hashes for rights categories and subrights
    
    * Added support for hashes for learn mores
    
    * deepL experimentation
    
    * added comments
    
    * removed json files, clean up
    
    * commented out experimentation files
    
    * Delete new-data.json
    
    * Removed console logging
    
    Co-authored-by: Alex Oseguera <aoseg@stanford.edu>

--- workers-rights-app/data/FetchRightsData.js
+++ workers-rights-app/data/FetchRightsData.js
@@ -1,5 +1,6 @@
 import * as firebase from "firebase";
-import firebaseConfig from "../constants/MyApiKeys";
+// import firebaseConfig from "../constants/MyApiKeys";
+import firebaseConfig from "../constants/MyApiKeysTest";
 import RightsCategory from "../models/rightsCategory";
 import SubRight from "../models/subRight";
 import Organization from "../models/organization";
@@ -41,7 +42,7 @@ export default class ImportedData {
     return this.learnMores;
   }
 
-  static async importAllData() {
+  static async importAllData(lang) {
     if (!firebase.apps.length) {
       // only load once
       firebase.initializeApp(firebaseConfig);
@@ -49,13 +50,23 @@ export default class ImportedData {
     // Get a reference to the database service
     var db = firebase.database();
 
+    // Set correct language for data
+    const supportedLanguages = ["en", "es"];
+    if(!supportedLanguages.includes(lang)) {
+      lang = "en"; // if not supported language, default to english
+    }
+
+    // Get correct strings for given language
+    // i.e. construct mapping like "1317869524" --> "Getting Hired" if selected language is English
+    let hashToPhrase = await constructHashToPhrase(db, lang);
+
     // Fetch rights categories
-    arr1 = await constructRightsCategories(db);
+    arr1 = await constructRightsCategories(db, hashToPhrase);
     arr1.sort((catA, catB) => (catA.id > catB.id ? 1 : -1)); // sort by cat id
     ImportedData.setRightsCategories(arr1);
 
     // Fetch subrights
-    arr2 = await constructSubrights(db);
+    arr2 = await constructSubrights(db, hashToPhrase);
     ImportedData.setSubRights(arr2);
 
     // Fetch organizations
@@ -63,23 +74,82 @@ export default class ImportedData {
     ImportedData.setOrganizations(arr3);
 
     // Fetch learn mores
-    arr4 = await constructLearnMores(db);
+    arr4 = await constructLearnMores(db, hashToPhrase);
     ImportedData.setLearnMores(arr4);
 
+    console.log("Finished fetching all data from firebase! Should be run FIRST"); // make sure async stuff works
     return; // return after all fetches are done
   }
 }
 
-function constructLearnMores(db) {
+/*
+Function that constructs mapping from hash to phrase in the language selected by the user
+(or defaulted to English if translation not available). Returns an array that looks like
+this: {"1317869524": "Getting Hired", "-936704732": "Safety & Healthy"} for easy lookup.
+*/
+function constructHashToPhrase(db, lang) {
+  let ref = db.ref("translation");
+  var hashToPhrase = {};
+
+  return ref.once("value").then(function (snapshot) {
+    snapshot.forEach(function (data) {
+      if (snapshot.child(data.key + "/" + lang).exists()) { // if lang translation exists
+        hashToPhrase[data.key] = data.val()[lang];
+      } else { // if no translation, default to english
+        // console.log("Lang doesn't exist! Defaulting to english.");
+        hashToPhrase[data.key] = data.val()["en"];
+      }
+    });
+    return hashToPhrase;
+  });
+}
+
+function constructRightsCategories(db, hashToPhrase) {
+  let ref = db.ref("rights-categories/");
+  var tempRightsCategories = [];
+
+  return ref.once("value").then(function (snapshot) {
+    snapshot.forEach(function (data) {
+      let img = getCategoryIcon(data); // get correct icon for ctegory
+      let temp = new RightsCategory(
+        data.val().id,
+        hashToPhrase[data.val().title], // get language sentence from hash 
+        img,
+        hashToPhrase[data.val().subtitle],
+        hashToPhrase[data.val().description]
+      );
+      tempRightsCategories.push(temp);
+    });
+    return tempRightsCategories;
+  });
+}
+
+/*
+Function that takes in json object of informationChunks containing hashes in the chunks,
+and returns a new version of informationChunks that contains the actual text instead of hashes.
+*/
+function getInformationChunksWithoutHashes(informationChunks, hashToPhrase) {
+  for(var elem in informationChunks) { // eg. 0, 1, 2
+    for(var chunk in informationChunks[elem]) { // eg. header, body
+        let chunkHash = informationChunks[elem][chunk];
+        informationChunks[elem][chunk] = hashToPhrase[chunkHash]; // replace by true string!
+    }
+  }
+  
+  return informationChunks;
+}
+
+function constructLearnMores(db, hashToPhrase) {
   let ref = db.ref("learn-mores/");
   var tempLearnMores = [];
 
   return ref.once("value").then(function (snapshot) {
+    
     snapshot.forEach(function (data) {
       let temp = new learnMore(
         data.key,
-        data.val().title,
-        data.val().informationChunks
+        hashToPhrase[data.val().title], 
+        getInformationChunksWithoutHashes(data.val().informationChunks, hashToPhrase) // data.val().informationChunks 
       );
       tempLearnMores.push(temp);
     });
@@ -107,7 +177,7 @@ function constructOrgs(db) {
   });
 }
 
-function constructSubrights(db) {
+function constructSubrights(db, hashToPhrase) {
   let ref = db.ref("subrights/");
   var tempSubrights = [];
 
@@ -116,11 +186,11 @@ function constructSubrights(db) {
       let temp = new SubRight(
         data.val().id,
         data.val().categoryIds,
-        data.val().title,
+        hashToPhrase[data.val().title],
         data.val().img,
         data.val().emoji,
         data.val().learnMores,
-        data.val().description,
+        hashToPhrase[data.val().description],
         data.val().organizations
       );
       tempSubrights.push(temp);
@@ -129,25 +199,6 @@ function constructSubrights(db) {
   });
 }
 
-function constructRightsCategories(db) {
-  let ref = db.ref("rights-categories/");
-  var tempRightsCategories = [];
-
-  return ref.once("value").then(function (snapshot) {
-    snapshot.forEach(function (data) {
-      let img = getCategoryIcon(data); // get correct icon for ctegory
-      let temp = new RightsCategory(
-        data.val().id,
-        data.val().title,
-        img,
-        data.val().subtitle,
-        data.val().description
-      );
-      tempRightsCategories.push(temp);
-    });
-    return tempRightsCategories;
-  });
-}
 
 // need to do this because require() needs a static string passed in...
 // using the data.val().image itself doesn't work...
