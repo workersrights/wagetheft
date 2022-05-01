f323611de0ec2502e2f9bceab8755c13ee229963
Alex Oseguera <aoseg@stanford.edu>
2022-04-30 23:03:53 -0700

    Created new organization class for updated backend

---
+++ workers-rights-app/models/Organization/organizationBuilder.js
@@ -0,0 +1,41 @@
+/* eslint-disable import/no-named-as-default */
+// eslint-disable-next-line import/no-named-as-default-member
+import Organization from "./organization";
+
+const buildOrg = (id, abbrev, addresses, description, image, name, website) => {
+  if (
+    id == null ||
+    abbrev == null ||
+    addresses == null ||
+    description == null ||
+    image == null ||
+    name == null ||
+    website == null
+  ) {
+    return null;
+  }
+
+  if (
+    typeof id !== "string" ||
+    typeof abbrev !== "string" ||
+    typeof addresses !== "object" ||
+    typeof description !== "string" ||
+    typeof image !== "string" ||
+    typeof name !== "string" ||
+    typeof website !== "string"
+  ) {
+    return null;
+  }
+
+  return new Organization(
+    id,
+    abbrev,
+    addresses,
+    description,
+    image,
+    name,
+    website
+  );
+};
+
+export default buildOrg;
